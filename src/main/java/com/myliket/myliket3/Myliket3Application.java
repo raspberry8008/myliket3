ab84d1950dcad1c6ee81a91dc8964c9a474722c8
raspberry8008 <shmyth4@gmail.com>
2022-08-26 15:21:56 +0900

    ADD : myliket3 프로젝트 생성

---
+++ src/main/java/com/myliket/myliket3/Myliket3Application.java
@@ -0,0 +1,13 @@
+package com.myliket.myliket3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myliket3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Myliket3Application.class, args);
+    }
+
+}
